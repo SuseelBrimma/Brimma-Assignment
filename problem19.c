a64aa34cd17a4afff7ff78d738d654de2b1f332e
SuseelBrimma <109515080+SuseelBrimma@users.noreply.github.com>
2022-07-21 12:12:00 +0530

    Create problem19.c

---
+++ problem19.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void){
+    int months[12] = { 31 , 28 , 31 , 30 , 31 , 30 , 31 , 31 , 30 , 31 , 30 , 31 };
+    int year=1901;
+    int dayoftheweek=2;  
+    int sunday=0;
+    int firstsundays=0;
+
+    while(year<=2000){
+        if(year%4==0)   
+            months[1]=29;
+
+        for (int i = 0; i < 12; ++i) {
+            for (int d = 1; d <= months[i]; d++){
+                if(dayoftheweek==7)  
+                    dayoftheweek=0;
+
+                if(dayoftheweek==sunday && d==1)
+                    firstsundays++;
+
+                dayoftheweek++;
+            }
+        }
+
+        months[1]=28;
+        year++;
+    }
+
+    printf("There are %d Sundays that fell on the first of the month\n", firstsundays);
+
+    return 0;
+}
