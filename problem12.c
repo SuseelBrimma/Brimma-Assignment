d730f02e35fc720dfc88f2b42e63bb89c9242bfc
SuseelBrimma <109515080+SuseelBrimma@users.noreply.github.com>
2022-08-01 08:21:42 +0530

    Create problem12.c

---
+++ problem12.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+static unsigned divisor_count(unsigned long n);
+
+int main(void)
+{
+  unsigned long i = 1, n = 1;
+
+  while (divisor_count(n) < 500) {
+    n += ++i;
+  }
+  printf("%lu\n", n);
+  return 0;
+}
+
+unsigned divisor_count(unsigned long n)
+{
+  unsigned ret = 1;
+  unsigned long i;
+
+  for (i = 2; i <= n; i++) {
+    unsigned c = 0;
+    while (n % i == 0) {
+      c++;
+      n /= i;
+    }
+    ret *= c+1;
+  }
+  return ret;
+}
